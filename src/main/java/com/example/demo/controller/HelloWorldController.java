1141b7df3570a6d693262f97927f6cf001bcf6c7
EAD\hsingh296 <er.harpreetsingh28@gmail.com>
2024-12-05 17:23:08 +0530

    updated file

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,14 +1,14 @@
-package com.example.demo.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HelloWorldController {
-
-	@GetMapping("/api/hello")
-    public String helloWorld() {
-        return "Hello World from Spring Boot API!";
-    }
-	
-}
+//package com.example.demo.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class HelloWorldController {
+//
+//	@GetMapping("/api/hello")
+//    public String helloWorld() {
+//        return "Hello World from Spring Boot API!";
+//    }
+//	
+//}
